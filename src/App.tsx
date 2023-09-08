43f2da515f70dc721ab04e89eacba48a1c33b4a4
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-09-08 11:32:52 +0800

    :sparkles: 增加切换深色/浅色模式按钮

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { onMounted, defineComponent } from 'vue'
+import { onMounted, defineComponent, provide, watch, type Ref } from 'vue'
 
 import { useWifiStore } from '@/stores/wifi'
 
@@ -6,12 +6,49 @@ import Main from './components/Main'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import { useStorage } from './hooks/useStorage'
+
+import { THEME } from './types/theme'
+
+import { themeKey, toggleThemeKey } from './symbols/theme'
+
 export default defineComponent({
   setup() {
     const wifiStore = useWifiStore()
     onMounted(async () => {
       await wifiStore.getWifiNameList()
       await wifiStore.getWifiDetailList()
+    })
+
+    //主題深色模式淺色模式切換
+    const theme = useStorage('theme', null) as Ref<THEME>
+    provide(themeKey, theme)
+    const toggleTheme = () => {
+      theme.value = theme.value == THEME.LIGHT ? THEME.DARK : THEME.LIGHT
+    }
+    provide(toggleThemeKey, toggleTheme)
+
+    watch(
+      theme,
+      (newVal) => {
+        if (newVal === THEME.DARK) {
+          document.documentElement.classList.add('dark')
+        } else {
+          document.documentElement.classList.remove('dark')
+        }
+      },
+      { immediate: true }
+    )
+
+    onMounted(() => {
+      if (
+        theme.value === THEME.DARK ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ) {
+        theme.value = THEME.DARK
+      } else {
+        theme.value = THEME.LIGHT
+      }
     })
 
     return () => (
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,25 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, inject } from 'vue'
+
+import { themeKey, toggleThemeKey } from '@/symbols/theme'
+import { THEME } from '@/types/theme'
 
 const ActionButton = defineComponent({
   props: {
     iconClass: {
       type: String,
       required: true
+    },
+    action: {
+      type: Function,
+      required: true
     }
   },
   setup(props) {
     return () => (
-      <button class="group flex items-center justify-center rounded-md p-1 transition-colors hover:bg-ice-cream-700/5 dark:hover:bg-ice-cream-200/10">
+      <button
+        class="group flex items-center justify-center rounded-md p-1 transition-colors hover:bg-ice-cream-700/5 dark:hover:bg-ice-cream-200/10"
+        onClick={() => props.action()}
+      >
         <i
           class={
             'ph  text-2xl text-slate-400 transition-colors group-hover:text-ice-cream-700 dark:text-neutral-400 dark:group-hover:text-ice-cream-200' +
@@ -22,20 +32,28 @@ const ActionButton = defineComponent({
   }
 })
 
-const actionButtonOption = [
-  {
-    iconClass: 'ph-sort-ascending'
-  },
-  { iconClass: 'ph-palette' },
-  { iconClass: 'ph-confetti' }
-]
-
 const Footer = defineComponent({
   setup() {
+    const theme = inject(themeKey)
+    const toggleTheme =
+      inject(toggleThemeKey) ||
+      (() => {
+        console.log('inject toggle theme failed')
+      })
+
+    const actionButtonOption = computed(() => [
+      {
+        iconClass: 'ph-sort-ascending',
+        action: () => console.log('sort')
+      },
+      { iconClass: theme?.value === THEME.DARK ? 'ph-moon' : 'ph-sun', action: toggleTheme },
+      { iconClass: 'ph-confetti', action: () => console.log('sort') }
+    ])
+
     return () => (
       <div class="fixed bottom-0 left-0 right-0 z-10  flex w-full items-center justify-center space-x-6 border-t border-slate-100 bg-white p-3 shadow-md shadow-slate-600/5 dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-neutral-950/40">
-        {actionButtonOption.map((bt) => (
-          <ActionButton iconClass={bt.iconClass}></ActionButton>
+        {actionButtonOption.value.map((bt) => (
+          <ActionButton iconClass={bt.iconClass} action={bt.action}></ActionButton>
         ))}
       </div>
     )
